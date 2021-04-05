9e280b3c25a2fe3169985ac07e503b75d9960017
unknown <jshigoodies@gmail.com>
2021-04-04 20:45:24 -0700

    Databases

--- jshisite/main/urls.py
+++ jshisite/main/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 path("", views.index, name="index"), 
 path("v1/", views.v1, name="view 1"),
+path("<int:id>", views.toDo, name="To Do by ID"),
+path("<str:name>", views.toDo2, name="To Do by NAME"),
 ]
--- jshisite/main/views.py
+++ jshisite/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
 def index(response):
@@ -7,3 +8,12 @@ def index(response):
 
 def v1(response):
     return HttpResponse("<h1>Hallo!</h1>")
+
+def toDo(reponse, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" %ls.id)
+
+def toDo2(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text))) # %s is a string or in python str
