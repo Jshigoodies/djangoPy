1782e2ade924c151e564692b644f8525cd97d2af
unknown <jshigoodies@gmail.com>
2021-05-15 13:29:04 -0700

    Html Templates

--- jshisite/main/urls.py
+++ jshisite/main/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.index, name="index"), 
-path("v1/", views.v1, name="view 1"),
-path("<int:id>", views.toDo, name="To Do by ID"),
-path("<str:name>", views.toDo2, name="To Do by NAME"),
+    path("", views.home, name="home"),
+
+    # path("", views.index, name="index"),
+    # path("v1/", views.v1, name="view 1"),
+    path("<int:id>", views.index, name="To Do List"),
+    # path("<str:name>", views.toDo2, name="To Do by NAME"),
 ]
--- jshisite/main/views.py
+++ jshisite/main/views.py
@@ -3,17 +3,20 @@ from django.http import HttpResponse
 from .models import ToDoList, Item
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Jshi :D</h1>")
+# def index(response):
+#     return HttpResponse("<h1>Jshi :D</h1>")
 
-def v1(response):
-    return HttpResponse("<h1>Hallo!</h1>")
-
-def toDo(reponse, id):
+# def v1(response):
+#     return HttpResponse("<h1>Hallo!</h1>")
+#
+def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>" %ls.id)
+    return render(response, "main/list.html", {"ls": ls})
+#
+# def toDo2(response, name):
+#     ls = ToDoList.objects.get(name=name)
+#     item = ls.item_set.get(id=1)
+#     return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text))) # %s is a string or in python str
 
-def toDo2(response, name):
-    ls = ToDoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text))) # %s is a string or in python str
+def home(response):
+    return render(response, "main/home.html", {})
